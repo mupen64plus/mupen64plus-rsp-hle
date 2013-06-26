bd832b00cd6d6e3b77d0715c4aa06fdd2312a0af
Bobby Smiles <bobby.smiles32@gmail.com>
2013-06-26 23:24:45 +0200

    More cosmetics changes.

--- src/mp3.c
+++ src/mp3.c
@@ -371,6 +371,15 @@ static void matrixing_step(u16 dmem_src, u16 fifo1, u16 fifo2)
     t[5] = t[4] + t[5];
     t[9] = v[25] + t[10];
 
+    *(s16*)(mp3data+((fifo1 + 0x020))) = (s16)(t[14] + v[17]);
+    *(s16*)(mp3data+((fifo1 + 0x060))) = (s16)(t[9] - v[17] - t[6]);
+    *(s16*)(mp3data+((fifo1 + 0x0a0))) = (s16)(t[5] + v[18] - t[9]);
+    *(s16*)(mp3data+((fifo1 + 0x0e0))) = (s16)(t[13] - v[18]);
+    *(s16*)(mp3data+((fifo1 + 0x120))) = (s16)(v[19] - t[13]);
+    *(s16*)(mp3data+((fifo1 + 0x160))) = (s16)(t[11] - v[19] - t[5]);
+    *(s16*)(mp3data+((fifo1 + 0x1a0))) = (s16)(t[7] - t[11]);
+    *(s16*)(mp3data+((fifo1 + 0x1e0))) = (s16)t[15];
+    
     *(s16*)(mp3data+((fifo2 + 0x020))) = (s16)t[14];
     *(s16*)(mp3data+((fifo2 + 0x060))) = (s16)(t[10] - t[6]);
     *(s16*)(mp3data+((fifo2 + 0x0a0))) = (s16)(t[4] - t[10] + v[18]);
@@ -380,14 +389,6 @@ static void matrixing_step(u16 dmem_src, u16 fifo1, u16 fifo2)
     *(s16*)(mp3data+((fifo2 + 0x1a0))) = (s16)(t[0] - v[24]);
     *(s16*)(mp3data+((fifo2 + 0x1e0))) = (s16)(-t[0]);
     
-    *(s16*)(mp3data+((fifo1 + 0x020))) = (s16)(t[14] + v[17]);
-    *(s16*)(mp3data+((fifo1 + 0x060))) = (s16)(t[9] - v[17] - t[6]);
-    *(s16*)(mp3data+((fifo1 + 0x0a0))) = (s16)(t[5] + v[18] - t[9]);
-    *(s16*)(mp3data+((fifo1 + 0x0e0))) = (s16)(t[13] - v[18]);
-    *(s16*)(mp3data+((fifo1 + 0x120))) = (s16)(v[19] - t[13]);
-    *(s16*)(mp3data+((fifo1 + 0x160))) = (s16)(t[11] - v[19] - t[5]);
-    *(s16*)(mp3data+((fifo1 + 0x1a0))) = (s16)(t[7] - t[11]);
-    *(s16*)(mp3data+((fifo1 + 0x1e0))) = (s16)t[15];
 }
 
 static void windowing_step(u16 dmem_dst, u16 fifo16_base, unsigned index)
