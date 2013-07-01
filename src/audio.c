f6fef4d3920c142e169a564364f2a71cfe5fdf5f
Bobby Smiles <bobby.smiles32@gmail.com>
2013-07-02 00:37:11 +0200

    Define 1080 acmd table.

--- src/audio.c
+++ src/audio.c
@@ -1480,6 +1480,16 @@ static const acmd_callback_t ABI_YS[0x18] =
     LOADBUFF2,  SAVEBUFF2,      ENVSETUP2,  UNKNOWN
 };
 
+static const acmd_callback_t ABI_1080[0x18] =
+{
+    UNKNOWN,    ADPCM2,         CLEARBUFF2, UNKNOWN,
+    ADDMIXER,   RESAMPLE2,      UNKNOWN,    FILTER2,
+    SETBUFF2,   DUPLICATE2,     DMEMMOVE2,  LOADADPCM2,
+    MIXER2,     INTERLEAVE2,    HILOGAIN,   SETLOOP2,
+    COPYBLOCKS2,INTERL2,        ENVSETUP1,  ENVMIXER2,
+    LOADBUFF2,  SAVEBUFF2,      ENVSETUP2,  UNKNOWN
+};
+
 /* global functions */
 void alist_process_ABI1()
 {
@@ -1530,7 +1540,8 @@ void alist_process_ys()
 
 void alist_process_1080()
 {
-    alist_process(&l_audio2, ABI2, 0x20);
+    // FIXME: no segments
+    alist_process(&l_audio2, ABI_1080, 0x18);
 }
 
 void alist_process_oot()
