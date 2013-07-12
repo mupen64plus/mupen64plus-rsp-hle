98d2b2839f6c9f324996e4c7d9ecd8ea6959a601
Bobby Smiles <bobby.smiles32@gmail.com>
2013-07-12 18:37:58 +0200

    Rename audio.{c,h} to alist.{c,h}

--- src/alist.c
+++ src/alist.c
@@ -1,5 +1,5 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *   Mupen64plus-rsp-hle - audio.c                                         *
+ *   Mupen64plus-rsp-hle - alist.c                                         *
  *   Mupen64Plus homepage: http://code.google.com/p/mupen64plus/           *
  *   Copyright (C) 2012 Bobby Smiles                                       *
  *   Copyright (C) 2009 Richard Goedeken                                   *
--- src/alist.h
+++ src/alist.h
@@ -1,5 +1,5 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *   Mupen64plus-rsp-hle - audio.h                                         *
+ *   Mupen64plus-rsp-hle - alist.h                                         *
  *   Mupen64Plus homepage: http://code.google.com/p/mupen64plus/           *
  *   Copyright (C) 2002 Hacktarux                                          *
  *                                                                         *
@@ -19,8 +19,8 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-#ifndef AUDIO_H
-#define AUDIO_H
+#ifndef ALIST_H
+#define ALIST_H
 
 void alist_process_ABI1();
 
