mupen64plus-ae/mupen64plus-ae

commit 3921889f3be47edb56b8ebc4691edbbd31d6b273

Share some ADPCM bits as well.

src/audio.c
@@ -19,7 +19,11 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+#include <assert.h>
+#include <stddef.h>
 #include <stdint.h>
+
+#include "hle.h"
 
 const int16_t RESAMPLE_LUT[64 * 4] = {
     0x0c39, 0x66ad, 0x0d46, 0xffdf, 0x0b39, 0x6696, 0x0e5f, 0xffd8,
@@ -55,4 +59,38 @@ const int16_t RESAMPLE_LUT[64 * 4] = {
     0xffc8, 0x10b4, 0x6626, 0x095a, 0xffd0, 0x0f83, 0x6669, 0x0a44,
     0xffd8, 0x0e5f, 0x6696, 0x0b39, 0xffdf, 0x0d46, 0x66ad, 0x0c39
 };
+
+static int32_t rdot(size_t n, const int16_t *x, const int16_t *y)
+{
+    int32_t accu = 0;
+
+    y += n;
+
+    while (n != 0) {
+        accu += *(x++) * *(--y);
+        --n;
+    }
+
+    return accu;
+}
+
+void adpcm_compute_residuals(int16_t* dst, const int16_t* src,
+        const int16_t* cb_entry, const int16_t* last_samples, size_t count)
+{
+    assert(count <= 8);
+
+    const int16_t* const book1 = cb_entry;
+    const int16_t* const book2 = cb_entry + 8;
+
+    const int16_t l1 = last_samples[0];
+    const int16_t l2 = last_samples[1];
+
+    size_t i;
+
+    for(i = 0; i < 8; ++i) {
+        int32_t accu = (int32_t)src[i] << 11;
+        accu += book1[i]*l1 + book2[i]*l2 + rdot(i, book2, src);
+        dst[i] = clamp_s16(accu >> 11);
+   }
+}
 

src/alist.c
@@ -202,15 +202,6 @@ void alist_resample(
 
 typedef unsigned int (*adpcm_predict_frame_t)(int16_t* dst, uint16_t dmemi, unsigned char scale);
 
-static int16_t adpcm_predict_sample(uint8_t byte, uint8_t mask,
-        unsigned lshift, unsigned rshift)
-{
-    int16_t sample = (uint16_t)(byte & mask) << lshift;
-    sample >>= rshift; /* signed */
-    return sample;
-}
-
-
 static unsigned int adpcm_predict_frame_4bits(int16_t* dst, uint16_t dmemi, unsigned char scale)
 {
     unsigned int i;
@@ -241,38 +232,6 @@ static unsigned int adpcm_predict_frame_2bits(int16_t* dst, uint16_t dmemi, unsi
     }
 
     return 4;
-}
-
-static int32_t rdot(size_t n, const int16_t *x, const int16_t *y)
-{
-    int32_t accu = 0;
-
-    y += n;
-
-    while (n != 0) {
-        accu += *(x++) * *(--y);
-        --n;
-    }
-
-    return accu;
-}
-
-static void adpcm_compute_residuals(int16_t* dst, const int16_t* src,
-        const int16_t* cb_entry, const int16_t* last_samples)
-{
-    const int16_t* const book1 = cb_entry;
-    const int16_t* const book2 = cb_entry + 8;
-
-    const int16_t l1 = last_samples[0];
-    const int16_t l2 = last_samples[1];
-
-    size_t i;
-
-    for(i = 0; i < 8; ++i) {
-        int32_t accu = (int32_t)src[i] << 11;
-        accu += book1[i]*l1 + book2[i]*l2 + rdot(i, book2, src);
-        dst[i] = clamp_s16(accu >> 11);
-   }
 }
 
 void alist_adpcm(
@@ -311,8 +270,8 @@ void alist_adpcm(
 
         dmemi += predict_frame(frame, dmemi, scale);
 
-        adpcm_compute_residuals(last_frame    , frame    , cb_entry, last_frame + 14);
-        adpcm_compute_residuals(last_frame + 8, frame + 8, cb_entry, last_frame + 6 );
+        adpcm_compute_residuals(last_frame    , frame    , cb_entry, last_frame + 14, 8);
+        adpcm_compute_residuals(last_frame + 8, frame + 8, cb_entry, last_frame + 6 , 8);
 
         for(i = 0; i < 16; ++i, dmemo += 2)
             *(int16_t*)(BufferSpace + (dmemo ^ S16)) = last_frame[i];

src/audio.h
@@ -26,4 +26,16 @@
 
 extern const int16_t RESAMPLE_LUT[64 * 4];
 
+
+static inline int16_t adpcm_predict_sample(uint8_t byte, uint8_t mask,
+        unsigned lshift, unsigned rshift)
+{
+    int16_t sample = (uint16_t)(byte & mask) << lshift;
+    sample >>= rshift; /* signed */
+    return sample;
+}
+
+void adpcm_compute_residuals(int16_t* dst, const int16_t* src,
+        const int16_t* cb_entry, const int16_t* last_samples, size_t count);
+
 #endif

src/musyx.c
@@ -137,15 +137,9 @@ static void adpcm_decode_frames(int16_t *dst, const uint8_t *src,
                                 const int16_t *table, uint8_t count,
                                 uint8_t skip_samples);
 
-static int16_t adpcm_get_predicted_sample(uint8_t byte, uint8_t mask,
-                                          unsigned lshift, unsigned rshift);
-static void adpcm_get_predicted_frame(int16_t *dst, const uint8_t *src,
-                                      const uint8_t *nibbles,
-                                      unsigned int rshift);
-static void adpcm_decode_upto_8_samples(int16_t *dst, const int16_t *src,
-                                        const int16_t *cb_entry,
-                                        const int16_t *last_samples,
-                                        size_t size);
+static void adpcm_predict_frame(int16_t *dst, const uint8_t *src,
+                                const uint8_t *nibbles,
+                                unsigned int rshift);
 
 static void mix_voice_samples(musyx_t *musyx, uint32_t voice_ptr,
                               const int16_t *samples, unsigned segbase,
@@ -164,21 +158,6 @@ static inline unsigned int align(unsigned int x, unsigned amount)
     --amount;
     return (x + amount) & ~amount;
 }
-
-static int32_t rdot(size_t n, const int16_t *x, const int16_t *y)
-{
-    int32_t accu = 0;
-
-    y += n;
-
-    while (n != 0) {
-        accu += ((int32_t)*(x++) * (int32_t)*(--y));
-        --n;
-    }
-
-    return accu;
-}
-
 
 static int32_t dot4(const int16_t *x, const int16_t *y)
 {
@@ -494,13 +473,13 @@ static void adpcm_decode_frames(int16_t *dst, const uint8_t *src,
         const int16_t *book = (c2 & 0xf0) + table;
         unsigned int rshift = (c2 & 0x0f);
 
-        adpcm_get_predicted_frame(frame, src, nibbles, rshift);
+        adpcm_predict_frame(frame, src, nibbles, rshift);
 
         memcpy(dst, frame, 2 * sizeof(frame[0]));
-        adpcm_decode_upto_8_samples(dst +  2, frame +  2, book, dst     , 6);
-        adpcm_decode_upto_8_samples(dst +  8, frame +  8, book, dst +  6, 8);
-        adpcm_decode_upto_8_samples(dst + 16, frame + 16, book, dst + 14, 8);
-        adpcm_decode_upto_8_samples(dst + 24, frame + 24, book, dst + 22, 8);
+        adpcm_compute_residuals(dst +  2, frame +  2, book, dst     , 6);
+        adpcm_compute_residuals(dst +  8, frame +  8, book, dst +  6, 8);
+        adpcm_compute_residuals(dst + 16, frame + 16, book, dst + 14, 8);
+        adpcm_compute_residuals(dst + 24, frame + 24, book, dst + 22, 8);
 
         if (jump_gap) {
             nibbles += 8;
@@ -514,17 +493,9 @@ static void adpcm_decode_frames(int16_t *dst, const uint8_t *src,
     }
 }
 
-static int16_t adpcm_get_predicted_sample(uint8_t byte, uint8_t mask,
-                                          unsigned lshift, unsigned rshift)
-{
-    int16_t sample = ((uint16_t)byte & (uint16_t)mask) << lshift;
-    sample >>= rshift; /* signed */
-    return sample;
-}
-
-static void adpcm_get_predicted_frame(int16_t *dst, const uint8_t *src,
-                                      const uint8_t *nibbles,
-                                      unsigned int rshift)
+static void adpcm_predict_frame(int16_t *dst, const uint8_t *src,
+                                const uint8_t *nibbles,
+                                unsigned int rshift)
 {
     unsigned int i;
 
@@ -534,29 +505,8 @@ static void adpcm_get_predicted_frame(int16_t *dst, const uint8_t *src,
     for (i = 1; i < 16; ++i) {
         uint8_t byte = nibbles[i];
 
-        *(dst++) = adpcm_get_predicted_sample(byte, 0xf0,  8, rshift);
-        *(dst++) = adpcm_get_predicted_sample(byte, 0x0f, 12, rshift);
-    }
-}
-
-static void adpcm_decode_upto_8_samples(int16_t *dst, const int16_t *src,
-                                        const int16_t *cb_entry,
-                                        const int16_t *last_samples,
-                                        size_t size)
-{
-    const int16_t *const book1 = cb_entry;
-    const int16_t *const book2 = cb_entry + 8;
-
-    const int16_t l1 = last_samples[0];
-    const int16_t l2 = last_samples[1];
-
-    size_t i;
-    int32_t accu;
-
-    for (i = 0; i < size; ++i) {
-        accu = (int32_t)src[i] << 11;
-        accu += book1[i] * l1 + book2[i] * l2 + rdot(i, book2, src);
-        dst[i] = clamp_s16(accu >> 11);
+        *(dst++) = adpcm_predict_sample(byte, 0xf0,  8, rshift);
+        *(dst++) = adpcm_predict_sample(byte, 0x0f, 12, rshift);
     }
 }
 

mupen64plus/mupen64plus-rsp-hle

commit 151fca618b45098180b4aa6eb9e13dc8be64400f

Fix regression introduced by commit 3921889f3be47edb56b8ebc4691edbbd31d6b273
adpcm_compute_residuals should honor the count parameter.

src/audio.c
@@ -87,7 +87,7 @@ void adpcm_compute_residuals(int16_t* dst, const int16_t* src,
 
     size_t i;
 
-    for(i = 0; i < 8; ++i) {
+    for(i = 0; i < count; ++i) {
         int32_t accu = (int32_t)src[i] << 11;
         accu += book1[i]*l1 + book2[i]*l2 + rdot(i, book2, src);
         dst[i] = clamp_s16(accu >> 11);