2f9a16cb3c4c8cec0886eaca063cc99a45cb75d6
Bobby Smiles <bobby.smiles32@gmail.com>
2017-06-02 15:11:13 +0200

    Fix unused variable warning.

--- src/re2.c
+++ src/re2.c
@@ -33,7 +33,6 @@
 void resize_bilinear_task(struct hle_t* hle)
 {
     int data_ptr = *dmem_u32(hle, TASK_UCODE_DATA);
-    int data_size = *dmem_u32(hle, TASK_UCODE_DATA_SIZE);
 
     int src_addr = *dram_u32(hle, data_ptr);
     int dst_addr = *dram_u32(hle, data_ptr + 4);
@@ -41,7 +40,9 @@ void resize_bilinear_task(struct hle_t* hle)
     int dst_height = *dram_u32(hle, data_ptr + 12);
     int x_ratio = *dram_u32(hle, data_ptr + 16);
     int y_ratio = *dram_u32(hle, data_ptr + 20);
+#if 0 /* unused, but keep it for documentation purpose */
     int dst_stride = *dram_u32(hle, data_ptr + 24);
+#endif
     int src_offset = *dram_u32(hle, data_ptr + 36);
 
     int a, b, c ,d, index, y_index, xr, yr, blue, green, red, addr, i, j;
@@ -58,7 +59,7 @@ void resize_bilinear_task(struct hle_t* hle)
         one_min_y_diff = 65536 - y_diff;
         y_index = yr * 320;
         x = 0;
-        
+
         for(j = 0; j < dst_width; j++)
         {
             xr = (int)(x >> 16);
@@ -66,29 +67,29 @@ void resize_bilinear_task(struct hle_t* hle)
             one_min_x_diff = 65536 - x_diff;
             index = y_index + xr;
             addr = src_addr + (index * 3);
-            
+
             dram_load_u8(hle, (uint8_t*)&a, addr, 3);
             dram_load_u8(hle, (uint8_t*)&b, (addr + 3), 3);
             dram_load_u8(hle, (uint8_t*)&c, (addr + (320 * 3)), 3);
             dram_load_u8(hle, (uint8_t*)&d, (addr + (320 * 3) + 3), 3);
-            
+
             blue = (int)(((a&0xff)*one_min_x_diff*one_min_y_diff + (b&0xff)*x_diff*one_min_y_diff +
                           (c&0xff)*y_diff*one_min_x_diff         + (d&0xff)*x_diff*y_diff) >> 32);
-            
+
             green = (int)((((a>>8)&0xff)*one_min_x_diff*one_min_y_diff + ((b>>8)&0xff)*x_diff*one_min_y_diff +
                            ((c>>8)&0xff)*y_diff*one_min_x_diff         + ((d>>8)&0xff)*x_diff*y_diff) >> 32);
-            
+
             red = (int)((((a>>16)&0xff)*one_min_x_diff*one_min_y_diff + ((b>>16)&0xff)*x_diff*one_min_y_diff +
                          ((c>>16)&0xff)*y_diff*one_min_x_diff         + ((d>>16)&0xff)*x_diff*y_diff) >> 32);
-            
+
             blue = (blue >> 3) & 0x001f;
             green = (green >> 3) & 0x001f;
             red = (red >> 3) & 0x001f;
             pixel = (red << 11) | (green << 6) | (blue << 1) | 1;
-            
+
             dram_store_u16(hle, &pixel, dst_addr, 1);
             dst_addr += 2;
-            
+
             x += x_ratio;
         }
         y += y_ratio;
